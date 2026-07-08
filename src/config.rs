9b3ded480c1e786ef2782a55d07e3d76199627f5
shafqat1841 <shafqat1841@gmail.com>
2026-07-08 20:43:18 +0500

    feat: add refresh token secret and max age env

--- src/config.rs
+++ src/config.rs
@@ -3,6 +3,8 @@ pub struct Config {
     pub database_url: String,
     pub jwt_secret: String,
     pub jwt_maxage: i64,
+    pub refresh_jwt_secret: String,
+    pub refresh_jwt_maxage: i64,
     pub port: u16,
 }
 
@@ -12,10 +14,15 @@ impl Config {
         let jwt_secret = std::env::var("JWT_SECRET_KEY").expect("JWT_SECRET_KEY must be set");
         let jwt_maxage = std::env::var("JWT_MAXAGE").expect("JWT_MAXAGE must be set");
 
+        let refresh_jwt_secret = std::env::var("REFRESH_JWT_SECRET_KEY").expect("REFRESH_JWT_SECRET_KEY must be set");
+        let refresh_jwt_maxage = std::env::var("REFRESH_JWT_MAXAGE").expect("REFRESH_JWT_MAXAGE must be set");
+
         Config {
             database_url,
             jwt_secret,
             jwt_maxage: jwt_maxage.parse::<i64>().unwrap(),
+            refresh_jwt_secret,
+            refresh_jwt_maxage: refresh_jwt_maxage.parse::<i64>().unwrap(),
             port: 8000,
         }
     }
