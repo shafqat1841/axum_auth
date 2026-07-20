85bd9688bfac1e8c5f44e1e2ebf8b5f1c0d9d458
shafqat1841 <shafqat1841@gmail.com>
2026-07-20 16:13:00 +0500

    fix: add ConfigTrait trait with init interface so that we can use that for production and testing and also transfer the previous init logic into parse_env so that the code is DRY

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,8 @@
 use anyhow::{Context, Result};
 
+pub trait ConfigTrait {
+    fn init() -> Result<Config>;
+}
 #[derive(Debug, Clone)]
 pub struct Config {
     pub database_url: String,
@@ -11,23 +14,33 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn init() -> Result<Config> {
+    // Private helper to avoid code duplication
+    pub fn parse_env() -> Result<Config> {
         let database_url = std::env::var("DATABASE_URL").context("DATABASE_URL must be set")?;
         let jwt_secret = std::env::var("JWT_SECRET_KEY").context("JWT_SECRET_KEY must be set")?;
-        let jwt_maxage = std::env::var("JWT_MAXAGE").context("JWT_MAXAGE must be set")?;
+        let jwt_maxage = std::env::var("JWT_MAXAGE")?
+            .parse::<i64>()
+            .context("Invalid JWT_MAXAGE")?;
 
-        let refresh_jwt_secret =
-            std::env::var("REFRESH_JWT_SECRET_KEY").context("REFRESH_JWT_SECRET_KEY must be set")?;
-        let refresh_jwt_maxage =
-            std::env::var("REFRESH_JWT_MAXAGE").context("REFRESH_JWT_MAXAGE must be set")?;
+        let refresh_jwt_secret = std::env::var("REFRESH_JWT_SECRET_KEY")
+            .context("REFRESH_JWT_SECRET_KEY must be set")?;
+        let refresh_jwt_maxage = std::env::var("REFRESH_JWT_MAXAGE")?
+            .parse::<i64>()
+            .context("Invalid REFRESH_JWT_MAXAGE")?;
 
         Ok(Config {
             database_url,
             jwt_secret,
-            jwt_maxage: jwt_maxage.parse::<i64>().unwrap(),
+            jwt_maxage,
             refresh_jwt_secret,
-            refresh_jwt_maxage: refresh_jwt_maxage.parse::<i64>().unwrap(),
+            refresh_jwt_maxage,
             port: 8000,
         })
+    }
+}
+
+impl ConfigTrait for Config {
+    fn init() -> Result<Config> {
+        Self::parse_env()
     }
 }
