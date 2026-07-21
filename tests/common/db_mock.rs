feaa1ec9c82d73438102755810c598d5221bc84a
shafqat1841 <shafqat1841@gmail.com>
2026-07-21 19:15:07 +0500

    fix: remove unwanted and unsed seed_some_user and seed_user method of DBClientMock struct

--- tests/common/db_mock.rs
+++ tests/common/db_mock.rs
@@ -19,41 +19,6 @@ impl DBClientMock {
         let users = Arc::new(Mutex::new(Vec::new()));
         DBClientMock { users }
     }
-
-    // Optional helper to pre-populate mock data for your tests
-    pub async fn seed_user(&self, user: UserMock) {
-        // let mut users = self.users.lock().await;
-        // users.push(user);
-        let _ = self
-            .save_user(user.username, user.email, user.password)
-            .await;
-    }
-
-    pub async fn seed_some_user(&self) {
-        let payload1 = UserMock {
-            username: "1testuser".to_string(),
-            email: "1test@example.com".to_string(),
-            password: "1SecurePassword123!".to_string(),
-        };
-
-        let payload2 = UserMock {
-            username: "2testuser".to_string(),
-            email: "2test@example.com".to_string(),
-            password: "2SecurePassword123!".to_string(),
-        };
-
-        let payload3 = UserMock {
-            username: "3testuser".to_string(),
-            email: "3test@example.com".to_string(),
-            password: "3SecurePassword123!".to_string(),
-        };
-
-        let users: [UserMock; 3] = [payload1, payload2, payload3];
-
-        for user_data in users {
-            self.seed_user(user_data).await;
-        }
-    }
 }
 
 impl DatabaseClient for DBClientMock {}
